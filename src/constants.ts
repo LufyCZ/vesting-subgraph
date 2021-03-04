6dd3ee3afc6650b8619bc89c6e584a59788ea40c
LufyCZ <lukasczk@seznam.cz>
2021-03-04 22:17:02 +0100

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+
+export const BIG_DECIMAL_ZERO = BigDecimal.fromString('0');
+export const BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18');
+
+export const BIG_INT_ZERO = BigInt.fromI32(0);
+
+export const DISTRIBUTOR = Address.fromString('0x84D1F7202e0E7DAc211617017cA72a2cB5E2B955');
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,60 +1,61 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address, BigDecimal } from "@graphprotocol/graph-ts"
 import {
   SushiMerkleDistributor,
   Claimed,
   MerkleRootUpdated,
   OwnershipTransferred
 } from "../generated/SushiMerkleDistributor/SushiMerkleDistributor"
-import { ExampleEntity } from "../generated/schema"
+import { User, Claim, Week } from "../generated/schema"
+import { BIG_DECIMAL_ZERO, BIG_DECIMAL_1E18, BIG_INT_ZERO, DISTRIBUTOR } from './constants';
 
-export function handleClaimed(event: Claimed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function getUser(address: Address): User {
+    let id = address.toHexString();
+    let user = User.load(id);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    if (user === null) {
+        user = new User(id);
+        user.totalClaimed = BIG_DECIMAL_ZERO;
+        user.save();
+    }
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.index = event.params.index
-  entity.account = event.params.account
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.frozen(...)
-  // - contract.isClaimed(...)
-  // - contract.merkleRoot(...)
-  // - contract.owner(...)
-  // - contract.pendingOwner(...)
-  // - contract.token(...)
-  // - contract.week(...)
+    return user as User;
 }
 
-export function handleMerkleRootUpdated(event: MerkleRootUpdated): void {}
+export function getWeek(weekNumber: BigInt): Week {
+    let id = weekNumber.toString();
+    let week = Week.load(id);
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+    if (week === null) {
+        let Distributor = SushiMerkleDistributor.bind(DISTRIBUTOR);
+
+        week = new Week(id);
+        week.totalClaimed = BIG_DECIMAL_ZERO;
+        week.numberOfClaims = BIG_INT_ZERO;
+        week.merkleRoot = Distributor.merkleRoot().toHexString();
+        week.save();
+    }
+
+    return week as Week;
+}
+
+export function handleClaimed(event: Claimed): void {
+    let claim = new Claim(event.params.account.toHexString() + '-' + event.params.week.toString());
+    let user = getUser(event.params.account);
+
+    claim.user = user.id.toString();
+    claim.week = event.params.week.toI32();
+    claim.amount = event.params.amount.toBigDecimal().div(BIG_DECIMAL_1E18);
+
+    user.totalClaimed = user.totalClaimed.plus(claim.amount);
+
+    claim.save();
+    user.save();
+
+
+    let week = getWeek(event.params.week);
+
+    week.totalClaimed = week.totalClaimed.plus(claim.amount);
+    week.numberOfClaims = week.numberOfClaims.plus(BigInt.fromI32(1));
+
+    week.save();
+}
